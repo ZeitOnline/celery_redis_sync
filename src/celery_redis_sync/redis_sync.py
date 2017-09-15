1b6a12b9e227054df6d39e89c067e92358eb80f2
Dominik Hoppe <dominik.hoppe@zeit.de>
2017-09-15 15:27:02 +0200

    Provide sync backend with redis connection

--- src/celery_redis_sync/redis_sync.py
+++ src/celery_redis_sync/redis_sync.py
@@ -1,14 +1,49 @@
 # -*- coding: utf-8 -*-
 """Synchronous Redis result store backend."""
+
+from kombu.utils.objects import cached_property
 import celery.backends.base
+import redis
 
 
-class RedisBackend(celery.backends.base.BaseKeyValueStoreBackend):
+class SynchronousRedisBackend(celery.backends.base.KeyValueStoreBackend):
     """Synchronous Redis result backend."""
 
-    def __init__(self, url=None, *args, **kwargs):
-        super(RedisBackend, self).__init__(*args, **kwargs)
+    #: :pypi:`redis` client module.
+    redis = redis
+
+    def __init__(self, url=None, connection_pool=None, *args, **kwargs):
+        super(SynchronousRedisBackend, self).__init__(*args, **kwargs)
         self.url = url
+
+        _get = self.app.conf.get
+        self._ConnectionPool = connection_pool
+
+        self.connparams = {
+            'host': _get('redis_host') or 'localhost',
+            'port': _get('redis_port') or 6379,
+            'db': _get('redis_db') or 0,
+            # 'password': _get('redis_password'),
+            # 'max_connections': self.max_connections,
+            # 'socket_timeout': socket_timeout and float(socket_timeout),
+            # 'socket_connect_timeout':
+            #    socket_connect_timeout and float(socket_connect_timeout),
+        }
+
+    def _create_client(self, **params):
+        return self.redis.StrictRedis(
+            connection_pool=self.ConnectionPool(**params),
+        )
+
+    @property
+    def ConnectionPool(self):
+        if self._ConnectionPool is None:
+            self._ConnectionPool = self.redis.ConnectionPool
+        return self._ConnectionPool
+
+    @cached_property
+    def client(self):
+        return self._create_client(**self.connparams)
 
     def get(self, key):
         return self.client.get(key)

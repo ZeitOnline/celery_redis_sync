3ec31074386cd3de4b08fb9cdf512cc06d37cbce
Dominik Hoppe <dominik.hoppe@zeit.de>
2017-09-20 15:00:47 +0200

    Enable worker, providing ping task

--- src/celery_redis_sync/conftest.py
+++ src/celery_redis_sync/conftest.py
@@ -19,11 +19,16 @@ def celery_test_app(redis_server):
     app.conf.update(redis_host=dsn['host'])
     app.conf.update(redis_port=dsn['port'])
     app.conf.update(redis_db=dsn['db'])
-    # XXX commented for debugging
-    # worker = celery.contrib.testing.worker.start_worker(app)
-    # worker.__enter__()
+    worker = celery.contrib.testing.worker.start_worker(app)
+    worker.__enter__()
     with celery.contrib.testing.app.setup_default_app(app):
         app.set_current()
         yield app
-        # XXX cleanup needs to be placed somewhere else
-        # worker.__exit__(None, None, None)
+        worker.__exit__(None, None, None)
+
+
+# celery.contrib.testing.worker expects a 'ping' task, so it can check that the
+# worker is running properly.
+@celery.shared_task(name='celery.ping')
+def celery_ping():
+    return 'pong'

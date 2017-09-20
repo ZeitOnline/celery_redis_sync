b84babd38d578e8b02d398ccfb5bdf6309e8279f
Dominik Hoppe <dominik.hoppe@zeit.de>
2017-09-20 15:31:29 +0200

    Extend simple test

--- src/celery_redis_sync/tests/test_redis.py
+++ src/celery_redis_sync/tests/test_redis.py
@@ -6,6 +6,9 @@ def test_task():
     return 42
 
 
-def test_redis_should_set_job_id(celery_test_app):
+def test_redis_should_execute_task(celery_test_app):
     res = test_task.delay()
-    res.state
+    # Need to call get() preventing a race condition, the test being too fast.
+    res.get()
+    assert res.state == 'SUCCESS'
+    assert res.result == 42

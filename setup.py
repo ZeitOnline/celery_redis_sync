1f1c12f132e7505dae632367328f24bb1ae8206a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2026-07-27 08:57:32 +0200

    Back to development: 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='celery_redis_sync',
-    version='1.1.2',
+    version='1.2.0.dev0',
     author='Die ZEIT Online Engineering',
     author_email='zon-backend@zeit.de',
     url='https://github.com/zeitonline/celery_redis_sync',
